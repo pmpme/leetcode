f4f32086c206b3b359323275a6709b797eec8d4c
pmpme <graceiscoding@gmail.com>
2025-12-09 19:48:22 -0800

    leets

--- quest/ds-algo/1-linear-shoal/stackmono1-final-prices-with-a-special-discount-in-a-shop.py
+++ quest/ds-algo/1-linear-shoal/stackmono1-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,1 +1,12 @@
 # Q1. Final Prices With a Special Discount in a Shop
+from typing import List
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        
+        for i, discount in enumerate(prices):
+            while stack and discount <= prices[stack[-1]]:
+                prices[stack.pop()] -= discount
+            stack.append(i)
+        return prices
