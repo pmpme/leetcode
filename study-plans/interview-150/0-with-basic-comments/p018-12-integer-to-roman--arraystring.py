8733e9bea271dc8433a63c42c537f59aba8cc0c3
pmpme <graceiscoding@gmail.com>
2025-12-27 12:38:23 -0800

    leets

--- study-plans/interview-150/0-with-basic-comments/p018-12-integer-to-roman--arraystring.py
+++ study-plans/interview-150/0-with-basic-comments/p018-12-integer-to-roman--arraystring.py
@@ -4,9 +4,31 @@
 from typing import List
 import pdb
 
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        codes = []
 
+        for val, code in self.get_vals():
+            while num >= val:
+                codes.append(code)
+                num -= val
 
-"""
-
-
-"""
+        return ''.join(codes)
+        
+        
+    def get_vals(self):
+        return [
+            (1000, 'M'),
+            (900, 'CM'),
+            (500, 'D'),
+            (400, 'CD'),
+            (100, 'C'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (10, 'X'),
+            (9, 'IX'),
+            (5, 'V'),
+            (4, 'IV'),
+            (1, 'I')
+        ]
